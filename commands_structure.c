0350685665852489a0fa8d236fe234afbf442693
Ella711 <allenro711@gmail.com>
2021-12-06 22:39:58 -0500

    [FIX] Betty Styling

--- commands_structure.c
+++ commands_structure.c
@@ -1,22 +1,27 @@
 #include "main.h"
 
+/**
+ * is_built_in - struct to find built in function
+ * @args: arguments
+ * @tokenized: tokenized line
+ * Return: NULL if not found
+ */
+
 int (*is_built_in(char **args))(char **tokenized)
 {
+size_t i = 0;
+builtin_t commands_built[] = {
+{"cd", exe_cd},
+{"help", exe_help},
+{"exit", exe_exit},
+{"env", exe_env},
+{NULL, NULL}};
 
-    size_t i = 0;
-    builtin_t commands_built[] = {
-        {"cd", exe_cd},
-        {"help", exe_help},
-        {"exit", exe_exit},
-		{"env", exe_env},
-        {NULL, NULL}};
-
-    while (commands_built[i].command != NULL)
-    {
-        if (strcmp(commands_built[i].command, args[0]) == 0) /*Future me pls change for own function*/
-            return (commands_built[i].command_func);
-        i++;
-    }
-
-    return (NULL);
+while (commands_built[i].command != NULL)
+{
+if (strcmp(commands_built[i].command, args[0]) == 0) /*change to own function*/
+return (commands_built[i].command_func);
+i++;
+}
+return (NULL);
 }
