e31ba801ac5ebe568110429048c0d7f053736839
Ella711 <allenro711@gmail.com>
2021-12-06 22:42:08 -0500

    [FIX] Betty Styling

--- built_ins.c
+++ built_ins.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * exe_cd - built in function for cd
+ * @args: arguments
+ * Return: 1
+ */
+
 int exe_cd(char **args)
 {
 	if (args[1] == NULL)
@@ -12,18 +18,39 @@ int exe_cd(char **args)
 	printf("%s\n", args[1]);
 	return (1);
 }
+
+/**
+ * exe_help - built in function for help
+ * @args: arguments
+ * Return: 1
+ */
+
 int exe_help(char **args)
 {
 	printf("Here goes a help proc\n");
 	printf("%s\n", args[0]);
 	return (1);
 }
+
+/**
+ * exe_exit - built in function for exit
+ * @args: arguments
+ * Return: 1
+ */
+
 int exe_exit(char **args)
 {
 	(void)(args);
 	printf("Here goes an Exit proc\n");
-	return(EXIT_SUCCESS);
+	return (EXIT_SUCCESS);
 }
+
+/**
+ * exe_env - built in function for env
+ * @args: arguments
+ * Return: 1
+ */
+
 int exe_env(char **args)
 {
 	int i = 0;
@@ -34,5 +61,5 @@ int exe_env(char **args)
 		printf("%s\n", environ[i]);
 		i++;
 	}
-	return(1);
+	return (1);
 }
